521a16b88fd423bcf90f4683eaf64fd4f0300b28
simon olsson <olssonsim@gmail.com>
2013-05-02 16:40:50 +0200

    Created FriendRequest class

---
+++ src/main/java/edu/chl/wmax/personalarm/model/entity/FriendRequest.java
@@ -0,0 +1,50 @@
+package edu.chl.wmax.personalarm.model.entity;
+
+import java.io.Serializable;
+import edu.chl.wmax.personalarm.model.entity.User;
+
+public class FriendRequest implements Serializable, IEntity<String> {
+
+	//@Id
+	//@NotNull
+	private Integer Id;
+	
+	private User sender;
+	private User receiver;
+	
+	// May be unnecessary.
+	public FriendRequest(User sender, User receiver) {
+		this.setSender(sender);
+		this.setReceiver(receiver);
+		// Set id?
+	}
+	
+	@Override
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setId(String id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public User getSender() {
+		return sender;
+	}
+
+	public void setSender(User sender) {
+		this.sender = sender;
+	}
+
+	public User getReceiver() {
+		return receiver;
+	}
+
+	public void setReceiver(User receiver) {
+		this.receiver = receiver;
+	}
+
+}
