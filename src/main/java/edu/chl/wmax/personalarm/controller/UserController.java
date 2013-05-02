336a48df16d05ada997ed732e25b11f94de1a6a7
Max Witt <kirayatail@gmail.com>
2013-05-02 23:10:07 +0200

    Expanded the UserController

--- src/main/java/edu/chl/wmax/personalarm/controller/UserController.java
+++ src/main/java/edu/chl/wmax/personalarm/controller/UserController.java
@@ -3,9 +3,42 @@ package edu.chl.wmax.personalarm.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import edu.chl.wmax.personalarm.model.entity.User;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
+
+	public String create() {
+
+		return null;
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@ResponseBody
+	public String list() {
+
+		return "{user: //list all users }";
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	@ResponseBody
+	public String show(@PathVariable int ident) {
+
+		return "{user: {id: " + ident + " } }";
+	}
 	
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	@ResponseBody
+	public String add(User usr) {
+
+		return null;
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	@ResponseBody
+	public String remove() {
+
+		return null;
+	}
 }
